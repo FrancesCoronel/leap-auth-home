c8fea3dab6f395c03140055c6bd3ddefc337583b
FVCproductions <hello@fvcproductions.com>
2016-10-09 09:20:32 -0400

    it works 😅
    Signed-off-by: FVCproductions <hello@fvcproductions.com>

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# leap-auth-home
-Leap Motion Auth for Home Automation | Kent Hack Enough 2016 🔨
+![Logo](http://66.media.tumblr.com/tumblr_low43o3Bqd1ql201ao1_500.gif)
+
+# Jake The Dog Key Hand | Security Corp
+
+[![Github Issues](http://githubbadges.herokuapp.com/fvcproductions/leap-auth-home/issues.svg?style=flat-square)](https://github.com/fvcproductions/leap-auth-home/issues) [![Pending Pull-Requests](http://githubbadges.herokuapp.com/fvcproductions/leap-auth-home/pulls.svg?style=flat-square)](https://github.com/fvcproductions/leap-auth-home/pulls) [![License](http://img.shields.io/:license-mit-blue.svg?style=flat-square)](http://badges.mit-license.org)
+
+> Check it out at [jakethedogkeyhand.com](http://jakethedogkeyhand.com)!
+>
+> GitHub Repo is at [fvcproductions/leap-auth-home](https://github.com/fvcproductions/leap-auth-home)
+
+## Inspiration
+
+Jake the Dog has a key hand n
+
+## What it does
+
+Web app that uses LeapMotion to authenticate user trying to enter their home.
+
+## How we built it
+
+Using CodeKit, we hosted a web site and plugged in a lot of Leap Motion plugins along with the API.
+
+## Challenges we ran into
+
+Getting the Leap Motion hands to display properly was hard. We spent a lot of time working on features that we shouldn't have.
+
+## Accomplishments that we're proud of
+
+Getting it to work in only a few hours!
+
+## What we learned
+
+Different Leap Motion plugins.
+
+## What's next for Jake the Dog Key Hand | Security Corp
+
+Optimizing the UI, extending password combination capabilities (multiple numbers).
+
+## Installation
+
+- Use `CodeKit` to minify `CSS` and `JS` files
+- Make sure to have LeapMotion installed on your computer
+
+## Contributing
+
+> To get started...
+
+- 🍴 Fork this repo [here](https://github.com/fvcproductions/leap-auth-home#fork-destination-box)
+- 🔨 Hack away
+- 😊 Add yourself as a contributor
+- 🔧 Make a pull request [here](https://github.com/fvcproductions/leap-auth-home/compare)
+
+> Or just create an issue - any little bit of help counts! 😊
+
+- 😯 Create an issue [here](https://github.com/fvcproductions/leap-auth-home/issues)!
+
+## Credits
+
+[![FVCproductions](https://avatars1.githubusercontent.com/u/4284691?v=3&s=200)](http://fvcproductions.com) | [![Noltron000](https://avatars2.githubusercontent.com/u/19766927?v=3&s=200)](http://github.com/noltron000)
+:---:|:---:|
+[`fvcproductions`](http://fvcproductions.com) | [`noltron000`](http://github.com/noltron000)
---
+++ assets/js/leapauth.js
@@ -0,0 +1,132 @@
+var riggedHandPlugin;
+
+Leap.loop({
+        hand: function(hand) {
+            var label = hand.data('label');
+            if (!label) {
+                label = document.createElement('label');
+                document.body.appendChild(label);
+                /**
+                 * Here we set the label to show the hand type
+                 */
+                label.innerHTML = hand.type + " hand";
+                hand.data('label', label);
+            }
+            var handMesh = hand.data('riggedHand.mesh');
+            var screenPosition = handMesh.screenPosition(
+                hand.palmPosition,
+                riggedHandPlugin.camera
+            );
+            label.style.left = screenPosition.x + 'px';
+            label.style.bottom = screenPosition.y + 'px';
+        }
+    })
+    .use('riggedHand')
+    .use('handEntry')
+    .on('handLost', function(hand) {
+        var label = hand.data('label');
+        if (label) {
+            document.body.removeChild(label);
+            hand.data({ label: undefined });
+        }
+    })
+    .use('playback', {
+        recording: './assets/js/left-or-right-77fps.json.lz',
+        timeBetweenLoops: 1000
+    });
+
+riggedHandPlugin = Leap.loopController.plugins.riggedHand;
+
+var paused = false;
+var whichhand;
+var totalfinger = 0;
+var combination_cnt = 1;
+var totalcombination = 3;
+var authed = false;
+var verify = false;
+
+function enterPass() {
+    document.getElementById("firstnote").style.visibility = "visible";
+}
+
+function greeting() {
+    if (verify === true) {
+        document.getElementById("greeting").innerHTML = "Welcome home.";
+    }
+    document.body.style.backgroundColor = "#B86E00";
+    document.getElementById("firstnote").style.visibility = "hidden";
+    document.getElementById("greeting").style.visibility = "visible";
+    document.getElementById("button").style.visibility = "hidden";
+}
+
+function auth_check(total) {
+    if (combination_cnt <= totalcombination) {
+        switch (combination_cnt) {
+            case 1:
+                if (total === 8) {
+                    break;
+                } else if (total === 9) {
+                    break;
+                } else {
+                    return;
+                }
+            case 2:
+                if (total === 1) {
+                    break;
+                } else if (total === 3) {
+                    break;
+                } else {
+                    return;
+                }
+            case 3:
+                if (total === 6) {
+                    break;
+                } else if (total === 7) {
+                    verify = true;
+                    break;
+                } else {
+                    return;
+                }
+        }
+        combination_cnt++;
+    } else {
+        greeting();
+        authed = true;
+    }
+}
+
+// Setup Leap loop with frame callback function
+var controllerOptions = { enableGestures: true };
+
+Leap.loop(controllerOptions, function(frame) {
+    if (paused) {
+        return; // Skip this update
+    }
+    totalfinger = 0;
+
+    if (frame.hands.length > 0) {
+        for (var i = 0; i < frame.hands.length; i++) {
+            var hand = frame.hands[i];
+            if (hand.confidence > 0.4) {
+                whichhand = hand.type;
+            }
+        }
+    } else {
+
+    }
+    if (frame.pointables.length > 0) {
+        for (var j = 0; j < frame.pointables.length; j++) {
+            var pointable = frame.pointables[j];
+            if (pointable.extended === true) {
+                totalfinger++;
+            }
+        }
+    } else {
+
+    }
+    if (authed !== 1) {
+        auth_check(totalfinger);
+    } else if (authed === 1) {
+        return;
+    }
+});
